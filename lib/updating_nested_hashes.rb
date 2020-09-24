32fb65235cfa5b2e949525962334c412b078e95b
akam alam <arundelfootball23@gmail.com>
2020-09-24 22:14:03 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -50,11 +50,9 @@ def add_characters
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
-      },
-      :additional_characters => [ {name: "Prince Escalus"},
-      {name: "Apothecary"}
-        
-      ]
+      }
+      
+    
    }
 
    # Write code below that does the following programmatically:
@@ -66,7 +64,7 @@ def add_characters
    # Code your solution here:
 
 
-
+epic_tragedy[:additional_characters] = []
 
 
 
